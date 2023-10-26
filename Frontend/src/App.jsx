1e1c2e3da172c3ba64110a6a61e6ea167015e98d
Israa <israahasibismail@gmail.com>
2023-10-26 13:54:26 +0200

    Alla componenter klara

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './Components/Header/Header'
 import Homepage from './Components/Homepage/Homepage'
-
+import Footer from './Components/Footer/Footer'
+import About from './Components/About/About'
+import Booking from './Components/Booking/Booking'
+import Support from './Components/Support/Support'
+import Login from './Components/Login/Login'
+import Detail from './Components/Detail/Detail'
 
 
 
@@ -13,6 +18,11 @@ function App() {
         <Routes>
           <Route index element={<Homepage />} />
           <Route path='about' element={<About />} />
+          <Route path='booking' element={<Booking />} />
+          <Route path='support' element={<Support />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='detail' element={<Detail />} />
 
           {/* <Route path='update/:id' element={<Update />} /> */}
 
