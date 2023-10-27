b566c1e0b6bc66ce782d4651a26a739f08e6c604
Israa <israahasibismail@gmail.com>
2023-10-27 09:12:47 +0200

    småfix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,36 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './Components/Header/Header'
-import Homepage from './Components/Homepage/Homepage'
-import Footer from './Components/Footer/Footer'
-import About from './Components/About/About'
-import Booking from './Components/Booking/Booking'
-import Support from './Components/Support/Support'
-import Login from './Components/Login/Login'
-import Detail from './Components/Detail/Detail'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
+import Homepage from './Components/Homepage/Homepage';
+import About from './Components/About/About';
+import Booking from './Components/Booking/Booking';
+import Support from './Components/Support/Support';
+import Login from './Components/Login/Login';
+import Detail from './Components/Detail/Detail';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path='about' element={<About />} />
-          <Route path='booking' element={<Booking />} />
-          <Route path='support' element={<Support />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-          <Route path='detail' element={<Detail />} />
+      <div>
+        <Header />
 
-          {/* <Route path='update/:id' element={<Update />} /> */}
+        <div className="content">
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path='about' element={<About />} />
+            <Route path='booking' element={<Booking />} />
+            <Route path='support' element={<Support />} />
+            <Route path='login' element={<Login />} />
+            <Route path='detail' element={<Detail />} />
+          </Routes>
+        </div>
 
-        </Routes>
+        <Footer />
       </div>
-      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
