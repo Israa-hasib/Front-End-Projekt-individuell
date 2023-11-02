832cd295f500d9853b14c0f05f27ccbf9b747221
Israa <israahasibismail@gmail.com>
2023-11-02 16:46:42 +0100

    små fix

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -13,48 +13,48 @@ export default function App() {
       <MDBContainer className='p-4'>
         <MDBRow>
           <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'>Home</h5>
+            <h5 className='text-uppercase'></h5>
 
             <ul className='list-unstyled mb-0'>
               <li>
                 <a href='./' className='text-dark'>
-                  homepage
+                  Homepage
                 </a>
               </li>
             </ul>
           </MDBCol>
 
           <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase mb-0'>About</h5>
+            <h5 className='text-uppercase mb-0'></h5>
 
             <ul className='list-unstyled'>
               <li>
                 <a href='./About' className='text-dark'>
-                  about
+                  About
                 </a>
               </li>
             </ul>
           </MDBCol>
 
           <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'>Booking</h5>
+            <h5 className='text-uppercase'></h5>
 
             <ul className='list-unstyled mb-0'>
               <li>
                 <a href='./Booking' className='text-dark'>
-                  booking
+                  Booking
                 </a>
               </li>
             </ul>
           </MDBCol>
 
           <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase mb-0'>Support</h5>
+            <h5 className='text-uppercase mb-0'></h5>
 
             <ul className='list-unstyled'>
               <li>
                 <a href='./Support' className='text-dark'>
-                  support
+                  Support
                 </a>
               </li>
             </ul>
