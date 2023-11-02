2839bb9d301a341f111bedf1dee3ed8900c14651
Israa <israahasibismail@gmail.com>
2023-11-02 14:38:11 +0100

    helt klar med about, login, register, support

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -61,13 +61,6 @@ export default function App() {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-dark' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
-      </div>
     </MDBFooter>
   );
 }
